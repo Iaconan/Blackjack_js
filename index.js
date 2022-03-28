1aee6d337a4759e678e69fdd116bafeda5b303d6
Vincenzo Iacona <vincenzoiacona@hotmail.com>
2022-03-29 01:09:10 +0200

    aggiunto id su cards e sum_cards

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-let firstCard = 10
-let secondCard = 10
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
+let firstCard = 10;
+let secondCard = 10;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+// let sumEl = document.getElementById("sum-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el")
+
 
 function startGame(){
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " - " + secondCard
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -16,7 +23,10 @@ function startGame(){
         isAlive = false
     }
 
-    console.log(message)
+    messageEl.textContent = message
+    
+
+    // console.log(message)
 }
 
 
